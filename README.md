a8bff70c66cac8fbfd4e99b780df970b8618f15f
Erick Naves <ericknevesea@gmail.com>
2025-12-07 19:15:38 -0300

    prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 Projeto para prática de programação
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import styles from "../styles/styles.module.css";
 
 function Home() {
   return (
-  <div className={styles.container}>
-    <h1 className={styles.title}>Voleizin hj?</h1>
-  </div>
-);
+    <div className={styles.container}>
+      <h1 className={styles.title}>Voleizin hj?</h1>
+    </div>
+  );
 }
 
 export default Home;
